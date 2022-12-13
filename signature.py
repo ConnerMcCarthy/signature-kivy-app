8782192bcdad3cb09c528c1cbae35c565e3a9b79
Conner McCarthy <conner.mccarthy.97@gmail.com>
2022-12-13 15:53:36 -0500

    adding multiple comments

--- signature.py
+++ signature.py
@@ -11,27 +11,36 @@ from kivy.graphics import Color, Ellipse, Line
 from kivy.uix.floatlayout import FloatLayout
 
 Window.clearcolor = (1, 1, 1, 1)
+
+#Displayed on a 2160 wide monitor
 Window.size = (2160,400)
 Window.left = True
 Window.top = True
 
+#Sets up the canvas background for the app
 class CanvasWidget(Widget):
+    #Creates the first point of a Line object when screen is touched 
     def on_touch_down(self, touch):
         with self.canvas:
             Color(0, 0, 0)
+            #touch.ud means touch.update[name of object]
             touch.ud['line'] = Line(points=(touch.x, touch.y), width=4)
 
+    #Adds another point to the Line object when the touch is moved
     def on_touch_move(self, touch):
         if(touch.profile != ['pos'] ):
             touch.ud['line'].points += [touch.x, touch.y]
 
-
+#Builds the app
 class SignatureApp(App):
     def build(self):
+        
+        #Window options
         Window.borderless = True
         parent = FloatLayout()
         self.painter = CanvasWidget(width=400)
         
+        #Setting up done and clear buttons
         doneBtn = Button(text='Sign', font_size=48, background_color='blue', size_hint=(.15,1), pos_hint={'right': 1})
         doneBtn.bind(on_release=self.screenshot)
         doneBtn.bind(on_press=self.tempDisable)
@@ -47,14 +56,17 @@ class SignatureApp(App):
     def tempDisable(self, obj):
         obj.disabled = True
 
+    def clear(self, obj):
+        self.painter.canvas.clear()
+
+    #Calls shareX.exe to take a screenshot. Clears and disables the screen to prevent multiple screenshots
     def screenshot(self, obj):
         os.system('.\ShareX.exe -workflow "mailroom"')
         sleep(3)
         self.painter.canvas.clear()
         obj.disabled = False
 
-    def clear(self, obj):
-        self.painter.canvas.clear()
+
 
 if __name__ == '__main__':
     SignatureApp().run()
